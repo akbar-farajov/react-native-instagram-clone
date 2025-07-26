960cb2a9867217e14486e0fc36384321739937b1
akbarfarajov <akbar.farajov@fintlabs.com>
2025-07-26 15:59:29 +0400

    add supabase

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,30 @@
-import { Text, View } from "react-native";
+import Account from "@/components/Account";
+import Auth from "@/components/Auth";
+import { supabase } from "@/lib/supabase";
+import { Session } from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
+import { View } from "react-native";
 
-export default function Index() {
+export default function App() {
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      {session && session.user ? (
+        <Account key={session.user.id} session={session} />
+      ) : (
+        <Auth />
+      )}
     </View>
   );
 }
