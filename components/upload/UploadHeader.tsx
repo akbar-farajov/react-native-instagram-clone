80791ef53a248a5b9e8a36c4db4f09343dd609c0
akbarfarajov <akbar.farajov@fintlabs.com>
2025-07-29 08:10:24 +0400

    feat(upload): add close button functionality in UploadHeader component

--- components/upload/UploadHeader.tsx
+++ components/upload/UploadHeader.tsx
@@ -1,3 +1,5 @@
+import { Ionicons } from "@expo/vector-icons";
+import { router } from "expo-router";
 import React from "react";
 import { Pressable, Text, View } from "react-native";
 
@@ -12,6 +14,13 @@ export const UploadHeader: React.FC<UploadHeaderProps> = ({
 }) => {
   return (
     <View className="flex-row justify-between items-center mb-6">
+      <Pressable
+        onPress={() => {
+          router.back();
+        }}
+      >
+        <Ionicons name="close" size={30} color="black" />
+      </Pressable>
       <Text className="text-xl font-bold">Create a new post</Text>
       <Pressable onPress={onShare} disabled={loading}>
         <Text
